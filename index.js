968133493fd79cd4226d9d77010d49d157c49222
IMANZE <97724382+IMANZE@users.noreply.github.com>
2022-06-22 22:57:39 +0200

    Solved exercise 10

--- index.js
+++ index.js
@@ -215,14 +215,78 @@ pyramid has spaces on both the left and right hand sides
         ' ### '
         '#####' */
 
-        function pyramid(n) {
-          let space = " "
-          let char = "#"
-          for (let i = 0; i < n + 1; i++) {
-            for (let j = 1; j < i + 1; j++) {
-              console.log(space.repeat(n - j) + char.repeat(j * 2 - 1) + space.repeat(n - j), "|")
-            }
-            console.log("step", i + 1)
-          }
-        }
-        pyramid(6)
+function pyramid(n) {
+  let space = " ";
+  let char = "#";
+  for (let i = 0; i < n + 1; i++) {
+    for (let j = 1; j < i + 1; j++) {
+      console.log(
+        space.repeat(n - j) + char.repeat(j * 2 - 1) + space.repeat(n - j),
+        "|"
+      );
+    }
+    console.log("step", i + 1);
+  }
+}
+pyramid(6);
+
+/* 10) SPYRAL MATRIX
+
+Write a function that accepts an integer N
+and returns a NxN spiral matrix.
+
+--- Examples
+
+    matrix(2)
+        [[1, 2],
+        [4, 3]]
+    matrix(3)
+        [[1, 2, 3],
+        [8, 9, 4],
+        [7, 6, 5]]
+    matrix(4)
+        [[1, 2, 3, 4],
+        [12, 13, 14, 5],
+        [11, 16, 15, 6],
+        [10,  9,  8, 7]]
+
+*/
+function matrix(n) {
+  const results = [];
+  for (let i = 0; i < n; i++) {
+    results.push([]);
+  }
+  let counter = 1;
+  let startColumn = 0;
+  let endColumn = n - 1;
+  let startRow = 0;
+  let endRow = n - 1;
+  while (startColumn <= endColumn && startRow <= endRow) {
+    // Top row
+    for (let i = startColumn; i <= endColumn; i++) {
+      results[startRow][i] = counter;
+      counter++;
+    }
+    startRow++;
+    // Right column
+    for (let i = startRow; i <= endRow; i++) {
+      results[i][endColumn] = counter;
+      counter++;
+    }
+    endColumn--;
+    // Bottom row
+    for (let i = endColumn; i >= startColumn; i--) {
+      results[endRow][i] = counter;
+      counter++;
+    }
+    endRow--;
+    // start column
+    for (let i = endRow; i >= startRow; i--) {
+      results[i][startColumn] = counter;
+      counter++;
+    }
+    startColumn++;
+  }
+  return results;
+}
+console.log(matrix(4));
